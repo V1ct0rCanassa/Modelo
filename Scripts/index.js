aafb07f16f26032e66accbe714563dd32bde28c6
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2024-06-05 23:33:13 -0300

    Add files via upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,6 +3,9 @@ const img = document.querySelectorAll('#img img');
 
 let idx = 0;
 
+let botaoMenu = document.getElementById('botaoMenu');
+let barraLateral = document.getElementById('barralateral');
+
 function slide() {
     idx++;
     if (idx > img.length - 1) {
@@ -13,3 +16,29 @@ function slide() {
 }
 
 setInterval(slide, 3000);
+
+
+// Função para alternar a barra lateral
+function alternarBarra() {
+    event.preventDefault();
+    if (barraLateral.style.display === 'flex') {
+        barraLateral.style.display = 'none';
+        barraLateral.style.position = 'none';
+    } else {
+        barraLateral.style.display = 'flex';
+        botaoMenu.style.rotate = '180deg';
+    }
+}
+
+// Função para fechar a barra lateral
+function fecharBarra() {
+    barraLateral.style.display = 'none';
+}
+document.addEventListener('click', function(event) {
+    if (!barraLateral.contains(event.target) && !botaoMenu.contains(event.target)) {
+        fecharBarra();
+    }
+});
+// Eventos
+botaoMenu.addEventListener('click', alternarBarra);
+barraLateral.addEventListener('mouseleave', fecharBarra);
